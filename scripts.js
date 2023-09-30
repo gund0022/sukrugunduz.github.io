02a016b28f5aa836bd0085e4b421fb8c14415d0b
gund0022 <gund0022@algonquinlive.com>
2023-09-30 13:06:44 -0400

    added scripts.js

---
+++ scripts.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const loginForm = document.getElementById("login-form");
+
+    loginForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        
+        const usernameInput = document.getElementById("username");
+        const passwordInput = document.getElementById("password");
+
+        const username = usernameInput.value;
+        const password = passwordInput.value;
+
+        if (username === "sukru_gndz@hotmail.com" && password === "test") {
+            window.location.href = "dashboard.html"; // Doğru giriş yapıldığında yönlendirilecek sayfa
+        } else {
+            alert("Hatalı kullanıcı adı veya şifre. Lütfen tekrar deneyin.");
+        }
+    });
+});
